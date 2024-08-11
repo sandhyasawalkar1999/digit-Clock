66cc24dcbd77b226d9d84f5b7b4fda5973633069
sandhyasawalkar1999 <sandhyasawalkar1999@gmail.com>
2024-08-11 20:01:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// console.log("hello");
+
+setInterval(() => {
+  let hours = document.getElementById("hours");
+  let minutes = document.getElementById("minutes");
+  let seconds = document.getElementById("seconds");
+
+  let ampm = document.getElementById("ampm");
+
+  let hh = document.getElementById("hh");
+  let mm = document.getElementById("mm");
+  let ss = document.getElementById("ss");
+
+  let hr_dot = document.querySelector(".hr_dot");
+  let min_dot = document.querySelector(".min_dot");
+  let sec_dot = document.querySelector(".sec_dot");
+
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let am = h >= 12 ? "PM" : "AM";
+
+  //conver 24 hr clock to 12 hr clock
+
+  if (h > 12) {
+    h = h - 12;
+  }
+  //add Zeros before single digit number
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hours.innerHTML = h + "<br><br> <span> Hours </span>";
+  minutes.innerHTML = m  + "<br> <br> <span> Minutes </span>";
+  seconds.innerHTML = s  + "<br> <br> <span> Seconds </span>";
+  ampm.innerHTML = am;
+
+  //12 hr clock
+  hh.style.strokeDashoffset = 440 -(440 *h)/12;
+
+  //60 min clock
+   mm.style.strokeDashoffset = 440 -(440 * m)/60;
+
+   //60 sec clock
+   ss.style.strokeDashoffset = 440 -(440 *s)/60;
+
+  //  hr_dot.style.transform = "rotate(${h * 30} deg)";
+
+
+
+   hr_dot.style.transform = `rotate(${h*30}deg)`;
+   //360/12 = 30
+
+   min_dot.style.transform = `rotate(${m*6}deg)`;
+   // 360/60 = 6
+
+   sec_dot.style.transform = `rotate(${s*6}deg)`;
+   //360/60 = 6
+});
